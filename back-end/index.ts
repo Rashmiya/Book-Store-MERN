797492d65c5fac5150f0295781b0ba3801642d38
Rashmiya <rashmiyarafeek1200@gmail.com>
2024-09-24 11:32:34 +0530

    configure the mongoDB connection

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,15 +2,27 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import homeRoute from './src/routes/homeRoute';
+import mongoose from 'mongoose';
+import { config } from './src/utils/config';
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT;
+const port = config.port;
+const mongoDbUrl = config.mongoDbUrl;
 
 // manage routes
 app.use('/', homeRoute);
 
-// server connection
-app.listen(port, () => {
-  console.log(`App is listening port: ${port}`);
-});
+// mongoDB connection
+mongoose
+  .connect(mongoDbUrl)
+  .then(() => {
+    console.log('MongoDB Connected Successfully');
+    // server connection
+    app.listen(port, () => {
+      console.log(`App is listening port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log('Connection Error', err);
+  });
