2721d5a041ab9e0b236361ee273a8380acb8c240
Rashmiya <rashmiyarafeek1200@gmail.com>
2024-09-24 07:08:03 +0530

    create default http route(/)

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,12 +1,17 @@
 // starting point of the application
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
-
 const port = process.env.PORT;
 
+// default Http Route is slash(/)
+app.get('/', (req: Request, res: Response) => {
+  return res.send('Welcome to the Eagle Eye Book Store');
+});
+
+// server connection
 app.listen(port, () => {
   console.log(`App is listening port: ${port}`);
 });
